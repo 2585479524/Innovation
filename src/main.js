ed3ea1b9fd28c5e9002b717a6bb29923b1c8b28e
2585479524 <2585479524@qq.com>
2019-04-19 11:08:46 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,30 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import iView from 'iview'; // 导入组件库
-import 'iview/dist/styles/iview.css'; // 导入样式
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import store from './store/index.js'
-import Vuex from 'vuex'
-import echarts from 'echarts'
-
-
-Vue.use(ElementUI)
-Vue.use(iView)
-Vue.use(Vuex)
-Vue.use(echarts)
+import store from './store'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  echarts,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
