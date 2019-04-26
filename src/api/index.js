65a110023ac33ad77d31ccd771b9d8e401b996b9
tentenka <745049157@qq.com>
2019-04-26 22:13:01 +0800

    修改学生

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,8 @@
-export default {
-    url: "http://39.107.102.246",
-}
+// export default {
+//     url: "http://39.107.102.246",
+// }
+const domain = "http://39.107.102.246";
+const interfaces = {
+    getCourseList: domain + "/student/course/list"
+};
+module.exports = interfaces;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers/router'
 import store from './stores/store'
+import index from "./api"
 
 import iView from 'iview'; // 导入组件库
 import 'iview/dist/styles/iview.css'; // 导入样式
@@ -10,14 +11,15 @@ import 'element-ui/lib/theme-chalk/index.css'
 import echarts from 'echarts'
 
 Vue.config.productionTip = false
+Vue.prototype.$index = index
 
 Vue.use(ElementUI)
 Vue.use(iView)
 Vue.use(echarts)
 
 new Vue({
-  router,
-  store,
-  echarts,
-  render: h => h(App)
+    router,
+    store,
+    echarts,
+    render: h => h(App)
 }).$mount('#app')
