15564f1ef157a2f4d6b44446aaf52e5d3c31f745
2585479524 <2585479524@qq.com>
2019-06-23 14:26:42 +0800

    courseTeacher

--- src/components/classpage/personal/userteacher/courseTeacher.vue
+++ src/components/classpage/personal/userteacher/courseTeacher.vue
@@ -1,66 +1,67 @@
 <template>
-  <div id="courseTeacher">
+  <div id="app">
     <div class="newcon">
-      <div class="title">
-        <font size="2">课程名称</font>
-        <font color="red" size="4">*</font>：
-        <i-input type="text" class="form-control" style="width:150px" v-model="rowtemplate.Name"/>
-
-        <font size="2">课程标签</font>
-        <font color="red" size="4">*</font>：
-        <i-input type="text" class="form-control" style="width:150px" v-model="rowtemplate.Tag"/>
-
-        <font size="2">选择课程封面</font>
-        <font color="red" size="4">*</font>
-        ：
-        <el-upload
-          action="http://39.107.102.246/upload/course/image"
-          list-type="picture-card"
-          :on-preview="handlePictureCardPreview"
-          :on-remove="handleRemove"
-        >
-          <i class="el-icon-plus"></i>
-        </el-upload>
-        <el-dialog :visible.sync="dialogVisible">
-          <img width="100%" :src="dialogImageUrl" alt>
-        </el-dialog>
-        <div class="but">
-          <i-button style="color:white;background-color:#3d6ea7" v-on:click="Save">添加课程</i-button>
-        </div>
+      <font size="2">课程名称</font>
+      <font color="red" size="4">*</font>：
+      <i-input type="text" class="form-control" style="width:150px" v-model="rowtemplate.Name"/>
+      <font size="2">课程标签</font>
+      <font color="red" size="4">*</font>：
+      <i-select style="width:150px" v-model="rowtemplate.Tag">
+        <i-option v-for="item in cityList" :value="item.value" :key="item.value">{{  item.label }}</i-option>
+      </i-select>
+      <br>
+      <br>
+      <font size="2">选择课程封面</font>
+      <font color="red" size="4">*</font>：
+      <el-upload
+        action="https://jsonplaceholder.typicode.com/posts/"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove"
+      >
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog :visible.sync="dialogVisible">
+        <img width="100%" :src="dialogImageUrl" alt>
+      </el-dialog>
+      <div class="but">
+        <i-button style="color:white;background-color:#3d6ea7" v-on:click="Save">添加课程</i-button>
       </div>
-
-      <table cellspacing="0" class="table table-bordered table-striped text-center">
-        <thead></thead>
-        <tbody>
-          <tr v-for="row in rows " :key="row.index">
-            <td>
-              <router-link tag="a" :to="{path:'/teacher/coursevalue',query:{id:rows.id}}">
-                <font size="4" color="gray">{{row.name}}</font>
-              </router-link>
-            </td>
-            <td>
-              <img :src="row.courseImg" width="150px" height="100px">
-            </td>
-            <td>
-              <font size="4" color="gray">{{row.teacher}}</font>
-            </td>
-            <td>
-              <font size="4" color="gray">{{row.tag}}</font>
-            </td>
-            <td>
-              <i-button style="color:white;background-color:#ec6c6c" @click="Delete(row)">删除</i-button>&nbsp;
-              <i-button style="color:gray;background-color:aliceblue" @click="Edit(row)">修改</i-button>&nbsp;
-              <i-button style="color:white;background-color:#3d6ea7" @click="success(row)">确定</i-button>&nbsp;
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
+    <br>
+    <br>
+    <table cellspacing="0" class="table table-bordered table-striped text-center">
+      <thead></thead>
+      <tbody>
+        <tr v-for="row in rows " :key="row.index">
+          <td>
+            <router-link tag="a" :to="{path:'/teacher/coursevalue',query:{id:row.id}}">
+              <font size="4" color="gray">{{row.name}}</font>
+            </router-link>
+          </td>
+          <td>
+            <img :src="row.courseImg" width="150px" height="100px">
+          </td>
+          <td>
+            <font size="4" color="gray">{{row.teacher}}</font>
+          </td>
+          <td>
+            <font size="4" color="gray">{{row.tag}}</font>
+          </td>
+          <td>
+            <i-button style="color:white;background-color:#ec6c6c" @click="Delete(row)">删除</i-button>&nbsp;
+            <i-button style="color:gray;background-color:aliceblue" @click="Edit(row)" v-show="show">{{word}}</i-button>&nbsp;
+            <i-button style="color:white;background-color:#3d6ea7" @click="success(row)" v-show="isshow">确定课程</i-button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <br>
   </div>
 </template>
 
 <script>
-import qs from "qs";
 export default {
   data() {
     return {
@@ -69,15 +70,32 @@ export default {
       file: "",
       dialogImageUrl: "",
       dialogVisible: false,
-      fileList2: []
-    };
+      fileList2: [],
+      cityList: [
+                    {
+                        value: 'web前端',
+                        label: 'web前端'
+                    },
+                    {
+                        value: 'web后端',
+                        label: 'web后端'
+                    },],
+      word:'修改',
+      show:true,
+      isshow:false
+    }
   },
-  created() {
-    this.axios
-      .get("/teacher/course/list", {})
+     created() {
+       this.$router.push({path:'/teacher/coursevalue',query:{id:row.id}})
+
+       this.axios
+       .get("/teacher/course/list", {
+
+       })
       .then(response => {
         console.log(response.data);
         this.rows = response.data.data;
+        console.log(this.rows);
       })
       .catch(function(error) {
         console.log(error);
@@ -85,11 +103,12 @@ export default {
   },
   methods: {
     Save: function(event) {
-      if (this.rowtemplate.Name && this.rowtemplate.Tag != "") {
+      if (this.rowtemplate.Name != "") {
         //设置当前新增行的Id
         this.rowtemplate.Id = this.rows.length + 1;
         this.rows.push(this.rowtemplate);
       }
+      console.log(this.rowtemplate.Tag);
 
       this.axios
         .post("/teacher/course", {
@@ -106,6 +125,7 @@ export default {
       this.axios
         .get("/teacher/course/list", {})
         .then(response => {
+          console.log(response.data);
           this.rows = response.data.data;
         })
         .catch(function(error) {
@@ -113,23 +133,10 @@ export default {
         });
       //还原模板
       this.rowtemplate = { Id: 0, Name: "", Tag: "" };
-
-      // this.axios({
-      //   method: "post",
-      //   url: "https://jsonplaceholder.typicode.com/posts",
-      //   headers: {
-      //     "Content-Type": "application/x-www-form-urlencoded"
-      //   },
-      //   data: qs.stringify({
-      //     course: this.rows.id,
-      //     image: ,
-      //   })
-      // });
     },
     Edit: function(row) {
       this.rowtemplate.Name = row.name;
       this.rowtemplate.Tag = row.tag;
-
       this.show = false;
       this.isshow = true;
     },
@@ -146,21 +153,21 @@ export default {
         });
     },
     success: function(row) {
+      console.log(this.rows)
       let rowId = row.id;
-      this.axios
-
+      this.axios 
         .put("/teacher/course/" + rowId, {
           name: this.rowtemplate.Name,
           tag: this.rowtemplate.Tag
         })
         .then(response => {
           console.log(response.data);
+  
         })
         .catch(function(error) {
           console.log(error);
         });
       this.rowtemplate = { Id: 0, Name: "", Tag: "" };
-
       this.isshow = false;
       this.show = true;
     },
@@ -185,8 +192,28 @@ export default {
 </script>
 
 <style scoped>
+#app {
+  width: 0 auto;
+  height: 500px;
+  margin-top: -830px;
+  margin-left: 300px;
+}
+.table {
+  font-family: verdana, arial, sans-serif;
+  font-size: 12px;
+  width: 700px;
+  padding-top: 40px;
+}
+.table td {
+  padding: 7px;
+}
 .newcon {
-  text-align: left;
+  text-align: start;
+  padding-left: 50px;
+}
+.but {
+  padding-left: 500px;
+  margin-top: -80px;
 }
 </style>
 
